c455465df685ae9a9a1d4024e330ea0ab4a3db6e
qaidsolutions <qaid.solutions@gmail.com>
2025-11-06 20:47:53 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px', fontFamily: 'sans-serif' }}>
+      <h1>Q·AID</h1>
+      <p>Fair, Fast, Accessible Healthcare</p>
+      <p>Welcome to the Q·aid MVP Website!</p>
+      <a href="/admin/login" style={{ color: '#007B7F' }}>Go to Admin Login</a>
+    </div>
+  );
+}
