504ce92e696218d41583f0c0d8219ac2d4211b72
qaidsolutions <qaid.solutions@gmail.com>
2025-11-06 20:48:38 +0530

    Create login.js

---
+++ pages/admin/login.js
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px', fontFamily: 'sans-serif' }}>
+      <h2>Admin Login</h2>
+      <p>(Login system will be added soon)</p>
+    </div>
+  );
+}
